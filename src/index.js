99d4de9d96d1807ee057ce7450cf9447b6d7126f
Edsamn <eduardomsribas@gmail.comm>
2024-02-29 20:19:00 -0300

    retirada logica de tratativa de  CORS

--- src/index.js
+++ src/index.js
@@ -13,12 +13,6 @@ const users = [];
 const posts = [];
 const loggedUsers = [];
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 //criar usuário
 app.post("/createUser/crypto", validateUser, async (req, res) => {
   const data = req.body;
