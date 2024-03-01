dd058170efc9ac99852c3b44ddf4ce772e59a79d
Edsamn <eduardomsribas@gmail.comm>
2024-02-29 21:38:51 -0300

    corsOption teste

--- src/index.js
+++ src/index.js
@@ -6,7 +6,12 @@ import validatePost from "./middlewares/validatePost";
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+const corsOption = {
+  origin: ["https://trabalho-final-intro-backend.onrender.com"],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+};
+app.use(cors(corsOption));
 const port = 3333;
 
 const users = [];
