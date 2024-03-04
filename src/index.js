b07011b51a302889b677f4561afc65b27d5736c3
Edsamn <eduardomsribas@gmail.comm>
2024-03-04 15:47:06 -0300

    teste cors

--- src/index.js
+++ src/index.js
@@ -6,7 +6,12 @@ import validatePost from "./middlewares/validatePost";
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 const port = 3333;
 
 const users = [];
