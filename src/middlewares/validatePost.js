bc089e83a5ecd2d25e2ffebb95f02b463dd5f9ce
Edsamn <eduardomsribas@gmail.comm>
2024-03-04 16:05:15 -0300

    att middlewares

--- src/middlewares/validatePost.js
+++ src/middlewares/validatePost.js
@@ -1,11 +1,11 @@
 function validatePost(req, res, next) {
   const data = req.body;
 
-  if (data.title.length < 3) {
+  if (data.title?.length < 3) {
     return res.status(400).json({msg: "O título precisa ter 3 caracteres ou mais"});
   }
 
-  if (data.description.length < 3) {
+  if (data.description?.length < 3) {
     return res.status(400).json({msg: "A descrição precisa ter 3 caracteres ou mais"});
   }
 
--- src/middlewares/validateUser.js
+++ src/middlewares/validateUser.js
@@ -1,11 +1,11 @@
 function validateUser(req, res, next) {
   const data = req.body;
 
-  if (data.name.length < 2) {
+  if (data.name?.length < 2) {
     return res.status(400).json({msg: "O nome precisa ter 2 caracteres ou mais"});
   }
 
-  if (data.pass.length < 6) {
+  if (data.pass?.length < 6) {
     return res.status(400).json({msg: "A senha precisa ter 6 caracteres ou mais"});
   }
 
