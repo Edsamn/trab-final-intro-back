f2acf406e93963289765859999c67ff877e71d00
Edsamn <eduardomsribas@gmail.comm>
2024-03-04 15:19:53 -0300

    mudadas logicas de middleware

--- src/middlewares/validatePost.js
+++ src/middlewares/validatePost.js
@@ -1,11 +1,11 @@
 function validatePost(req, res, next) {
   const data = req.body;
 
-  if (data.title.length < 3 || data.title === undefined) {
+  if (data.title.length < 3) {
     return res.status(400).json({msg: "O título precisa ter 3 caracteres ou mais"});
   }
 
-  if (data.description.length < 3 || data.description === undefined) {
+  if (data.description.length < 3) {
     return res.status(400).json({msg: "A descrição precisa ter 3 caracteres ou mais"});
   }
 
--- src/middlewares/validateUser.js
+++ src/middlewares/validateUser.js
@@ -1,11 +1,11 @@
 function validateUser(req, res, next) {
   const data = req.body;
 
-  if (data.name.length < 2 || data.name === undefined) {
+  if (data.name.length < 2) {
     return res.status(400).json({msg: "O nome precisa ter 2 caracteres ou mais"});
   }
 
-  if (data.pass.length < 6 || data.pass === undefined) {
+  if (data.pass.length < 6) {
     return res.status(400).json({msg: "A senha precisa ter 6 caracteres ou mais"});
   }
 
